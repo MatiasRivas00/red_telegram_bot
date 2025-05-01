b9bf91c2e8d8f20cf89d00c83b6a1865c7cdda9c
Matias Rivas <matias.rivas.aa@gmail.com>
2025-05-01 01:52:05 -0400

    feat[handlers]: add new handlers

--- bot_webhook.py
+++ bot_webhook.py
@@ -1,33 +1,24 @@
 import logging
 import os
-import asyncio
 from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI, Request, Response
-from dotenv import load_dotenv # Opcional: para leer .env
+from dotenv import load_dotenv
 
 from telegram import Update
-from telegram.ext import (
-    Application,
-    CommandHandler,
-    ContextTypes,
-    ApplicationBuilder,
-)
+from telegram.ext import Application
 
-from services import red
-from parser import red_parser
+from handlers import prediction_handlers
 
-# --- Configuración ---
-load_dotenv() # Carga variables de .env si existe
+load_dotenv()
 
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
-PORT = int(os.environ.get("PORT", 8000)) 
-NGROK_URL = os.environ.get("NGROK_URL", "PON_AQUI_TU_URL_DE_NGROK_HTTPS") 
-SECRET_PATH = os.environ.get("SECRET_PATH", "tu_secreto_123") 
+PORT = int(os.environ.get("PORT")) 
+NGROK_URL = os.environ.get("NGROK_URL") 
+SECRET_PATH = os.environ.get("SECRET_PATH") 
 WEBHOOK_URL = f"{NGROK_URL}/{SECRET_PATH}" 
 
-# --- Configuración de Logging ---
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
@@ -35,138 +26,71 @@ logging.basicConfig(
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
-# --- Handler Simplificado ---
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+telegram_app: Application | None = None
 
-# --- New async function for sending predictions ---
-async def prediction_loop(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    try:
-        token = context.user_data.get('red_token')
-        for _ in range(10):
-            prediction = red.get_prediction(token, 'PI445')
-            parsed_prediction = red_parser.red_parser(prediction)
-            reply = red_parser.reply_text(parsed_prediction)
-            # await update.message.reply_text(prediction)
-            await update.message.reply_text(reply)
-            await asyncio.sleep(60)
-        await update.message.reply_text("💯 Finished sending predictions!")
-    except asyncio.CancelledError:
-        await update.message.reply_text("⛔️ Prediction updates have been cancelled.")
-        raise
+async def app_init():
+    global telegram_app
+    logger.info("Iniciando lifespan")
 
+    telegram_app = Application.builder().token(TELEGRAM_TOKEN).build()
 
-async def prediction(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # Check if there's already a prediction running
-    if 'prediction_task' in context.user_data and not context.user_data['prediction_task'].done():
-        await update.message.reply_text("⚠️ Prediction updates are already running!")
-        return
-    
-    # get token
-    token = red.get_token()
-    if not token:
-        await update.message.reply_text("🚫 Couldn't get a token from red.cl")
-        return
-    context.user_data['red_token'] = token
+    prediction_handlers.add_to(telegram_app)
 
-    await update.message.reply_text("✅ Starting prediction updates every minute for 10 minutes...")
-    
-    task = asyncio.create_task(prediction_loop(update, context))
-    context.user_data['prediction_task'] = task
+    await telegram_app.initialize()
 
-
-async def stop_prediction(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    task = context.user_data.get('prediction_task')
-    if task and not task.done():
-        task.cancel()
-        await update.message.reply_text("❗️ Cancelling prediction updates...")
-    else:
-        await update.message.reply_text("⚠️ No prediction updates are currently running.")
-
-# --- Lógica de FastAPI y PTB ---
-ptb_app: Application | None = None
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    """Gestiona inicio y parada de PTB."""
-    global ptb_app
-    logger.info("Iniciando lifespan...")
-
-    if not TELEGRAM_TOKEN:
-        logger.error("Error: BOT_TOKEN no configurado.")
-        return # No continuar si no hay token
-    if not NGROK_URL:
-        logger.error("Error: NGROK_URL (URL pública de ngrok) no configurada.")
-        return # No continuar si no hay URL pública
-
-    # Construir App PTB
-    ptb_app = Application.builder().token(TELEGRAM_TOKEN).build()
-
-    # Registrar ÚNICO handler
-    ptb_app.add_handler(CommandHandler("hello", hello))
-    ptb_app.add_handler(CommandHandler("prediction", prediction))
-    ptb_app.add_handler(CommandHandler("stop_prediction", stop_prediction))
-
-
-    # Inicializar PTB
-    await ptb_app.initialize()
     logger.info("Aplicación PTB inicializada.")
 
-    # Establecer Webhook (apuntando a la URL pública de ngrok)
     try:
-        await ptb_app.bot.set_webhook(
+        await telegram_app.bot.set_webhook(
             url=WEBHOOK_URL,
             allowed_updates=Update.ALL_TYPES,
-            # Opcional: usar IP del servidor Nginx si ngrok y nginx están en máquinas distintas
-            # ip_address="IP.DE.TU.SERVIDOR.NGINX" 
         )
         logger.info(f"Webhook establecido en {WEBHOOK_URL}")
-        webhook_info = await ptb_app.bot.get_webhook_info()
+        webhook_info = await telegram_app.bot.get_webhook_info()
         logger.info(f"Confirmación Webhook: {webhook_info}")
-        if webhook_info.url != WEBHOOK_URL:
-             logger.warning("¡La URL del webhook establecida difiere de la configurada!")
 
     except Exception as e:
         logger.error(f"Error al establecer el webhook: {e}", exc_info=True)
-        # Podrías decidir detener la app aquí si el webhook falla
 
-    # Iniciar procesos de PTB (necesario para process_update)
-    await ptb_app.start()
+    await telegram_app.start()
     logger.info("Aplicación PTB iniciada (modo webhook).")
 
-    yield # FastAPI/Uvicorn se ejecuta aquí
-
-    # --- Limpieza al detener ---
-    logger.info("Deteniendo lifespan...")
-    if ptb_app:
-        logger.info("Deteniendo aplicación PTB...")
-        await ptb_app.stop()
+async def app_kill():
+    global telegram_app
+    logger.info("⏳ Deteniendo lifespan")
+    if telegram_app:
+        logger.info("⏳ Deteniendo aplicación PTB")
+        await telegram_app.stop()
         try:
-            logger.info("Intentando eliminar webhook...")
-            if await ptb_app.bot.delete_webhook():
-                logger.info("Webhook eliminado.")
+            logger.info("⏳ Intentando eliminar webhook")
+            if await telegram_app.bot.delete_webhook():
+                logger.info("✅ Webhook eliminado")
             else:
-                logger.warning("No se pudo eliminar el webhook (quizás ya no existía).")
+                logger.warning("⚠️ No se pudo eliminar el webhook")
         except Exception as e:
-            logger.error(f"Error eliminando webhook: {e}", exc_info=True)
-        await ptb_app.shutdown()
-        logger.info("Aplicación PTB detenida.")
+            logger.error(f"❌ Error eliminando webhook: {e}", exc_info=True)
+        await telegram_app.shutdown()
+        logger.info("✅Aplicación PTB detenida.")
 
-# Crear Instancia FastAPI
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app_init()
+    yield
+    app_kill()
+
 app = FastAPI(lifespan=lifespan)
 
 @app.post(f"/{SECRET_PATH}")
 async def telegram_webhook_endpoint(request: Request):
-    """Recibe actualizaciones de Telegram via POST."""
-    if not ptb_app:
+    if not telegram_app:
         logger.error("Webhook llamado pero PTB App no está lista.")
         return Response(status_code=503) # Service Unavailable
 
     try:
         data = await request.json()
-        update = Update.de_json(data, ptb_app.bot)
+        update = Update.de_json(data, telegram_app.bot)
         logger.info(f"Update recibido: {update.update_id}")
-        await ptb_app.process_update(update)
+        await telegram_app.process_update(update)
         return Response(status_code=200) # OK
     except Exception as e:
         logger.error(f"Error procesando update: {e}", exc_info=True)
@@ -177,14 +101,9 @@ async def health_check():
     """Verifica que el servidor FastAPI esté vivo."""
     return {"status": "FastAPI está corriendo"}
 
-# --- Punto de entrada (para desarrollo/pruebas locales) ---
 if __name__ == "__main__":
-    # Asegúrate de que las variables de entorno estén seteadas antes de correr
-    print(f"Verifica que BOT_TOKEN esté configurado.")
-    print(f"Verifica que NGROK_URL sea: {NGROK_URL}")
     print(f"Corriendo Uvicorn en http://127.0.0.1:{PORT}")
     print(f"El webhook se configurará en: {WEBHOOK_URL}")
-    print("Nginx debe estar corriendo y ngrok debe estar apuntando a Nginx.")
     
     uvicorn.run(
         "bot_webhook:app", 
---
+++ handlers/prediction_handlers.py
@@ -0,0 +1,115 @@
+import asyncio
+
+from telegram import Update
+from telegram.ext import ContextTypes, Application
+
+from services import red
+from parser import red_parser
+
+DEFAULT_DURATION = 10 # minutes
+DEFAULT_INTERVAL = 60 # seconds
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+async def prediction_loop(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    code = context.user_data.get("code")
+    interval = context.user_data.get("default_interval", DEFAULT_INTERVAL)
+    duration = context.user_data.get("default_duration", DEFAULT_DURATION)
+    token = context.user_data.get('red_token')
+
+    await update.message.reply_text(f"Starting prediction for {code} stop, during {duration} minutes, every {interval} seconds")
+
+    try:
+        for _ in range(duration*60//interval):
+            prediction = red.get_prediction(token, code)
+            parsed_prediction = red_parser.red_parser(prediction)
+            reply = red_parser.reply_text(parsed_prediction)
+            # await update.message.reply_text(prediction)
+            await update.message.reply_text(reply)
+            await asyncio.sleep(interval)
+        await update.message.reply_text("💯 Finished sending predictions!")
+    except asyncio.CancelledError:
+        await update.message.reply_text("⛔️ Prediction updates have been cancelled.")
+        raise
+
+
+async def prediction(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if 'prediction_task' in context.user_data and not context.user_data['prediction_task'].done():
+        await update.message.reply_text("⚠️ Prediction updates are already running!")
+        return
+    
+    token = red.get_token()
+    if not token:
+        await update.message.reply_text("🚫 Couldn't get a token from red.cl")
+        return
+    context.user_data['red_token'] = token
+
+    if len(context.args) != 0:
+        context.user_data['code']  = context.args[0]
+    elif context.user_data.get('default_code'):
+        context.user_data['code']  = context.user_data.get('default_code')
+    else:
+        await update.message.reply_text('⚠️ A code must be provided or setted with /default_code PI445 for example')
+    
+    task = asyncio.create_task(prediction_loop(update, context))
+    context.user_data['prediction_task'] = task
+
+
+async def stop_prediction(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    task = context.user_data.get('prediction_task')
+    if task and not task.done():
+        task.cancel()
+        await update.message.reply_text("❗️ Cancelling prediction updates...")
+    else:
+        await update.message.reply_text("⚠️ No prediction updates are currently running.")
+
+async def default_code(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    try:
+        code = context.args[0]
+        context.user_data["default_code"] = code
+        await update.message.reply_text(f"✅ Default code setted to {code}")
+    except:
+        await update.message.reply_text(f"❌ a code must be provided")
+
+async def default_duration(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    try:
+        duration = context.args[0]
+        context.user_data["default_duration"] = int(duration)
+        await update.message.reply_text(f"✅ Default duration setted to {duration} minutes")
+    except:
+        await update.message.reply_text(f"❌ invalid duration has been provided")
+
+async def default_interval(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    try:
+        interval = context.args[0]
+        context.user_data["default_range"] = int(interval)
+        await update.message.reply_text(f"✅ Default range setted to {interval} seconds")
+    except:
+        await update.message.reply_text(f"❌ invalid interval has been provided")
+
+async def info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    code = context.user_data.get("default_code")
+    interval = context.user_data.get("default_interval", DEFAULT_INTERVAL)
+    duration = context.user_data.get("default_duration", DEFAULT_DURATION)
+
+    await update.message.reply_text(f"ℹ️ Default code setted is {code}, interval is {interval} seconds, during {duration} minutes")
+
+def add_to(app: Application):
+    app.add_handler("prediction", prediction)
+    app.add_handler("p", prediction)
+
+    app.add_handler("stop_prediction", stop_prediction)
+    app.add_handler("s", stop_prediction)
+
+    app.add_handler("default_code", default_code)
+    app.add_handler("dc", default_code)
+
+    app.add_handler("default_interval", default_interval)
+    app.add_handler("di", default_interval)
+
+    app.add_handler("default_duration", default_duration)
+    app.add_handler("dd", default_duration)
+
+    app.add_handler("info", info)
+    app.add_handler("i", info)
